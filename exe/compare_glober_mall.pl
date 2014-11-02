b56ad609a91f02dfa45d68fbbc62fc6e5e6aec1f
Takashi Hashiguchi <takashi19880319@gmail.com>
2014-11-02 19:51:00 +0900

    バグ：差分に出てこないバズのものが出ていた →フィックス

--- exe/compare_glober_mall.pl
+++ exe/compare_glober_mall.pl
@@ -264,8 +264,13 @@ while($goods_line = <GOODS_FILE>){
 					if (length($rakuten_select_4_code) != 4) {
 						$rakuten_select_4_code = delete_double_quotation($rakuten_select_code[8]);
 					}
-					my $rakuten_select_5code=delete_double_quotation($rakuten_select_code[1]); 
+					my $rakuten_select_5code=delete_double_quotation($rakuten_select_code[1]);
 					my $rakuten_select_code="$rakuten_select_5code$rakuten_select_4_code";
+					if($rakuten_select_code =~ /"/){
+						my $before_rep_str0="\"";
+						my $after_rep_str0="";
+						$rakuten_select_code =~ s/$before_rep_str0/$after_rep_str0/g;
+					} 
 					if ($glober_goods_9code eq $rakuten_select_code) {
 						$find_flag=1;
 						last;
